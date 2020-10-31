adf44e4283a6ad3e85e42e7ac48c68a1bc8ba60c
jajurney <josephajurney@gmail.com>
2020-10-30 22:04:14 -0400

    first commit

---
+++ Outings_Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outings_Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProgramUI ui = new ProgramUI();
+            ui.Run();
+
+            DateTime dateOfEvent = new DateTime();
+            Console.WriteLine(dateOfEvent.ToString());
+        }
+    }
+}
---
+++ Outings_Console/ProgramUI.cs
@@ -0,0 +1,52 @@
+﻿using Outings_Repsitory;
+using Outings_Repsitory.Outings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outings_Console
+{
+    class ProgramUI
+    {
+        private OutingContent_Repo _repo = new OutingContent_Repo();
+        public void Run()
+        {
+            OutingsContent();
+            Menu();
+        
+        }
+        public void OutingsContent()
+        {
+            OutingContent golfOuting = new OutingContent(
+                "Golf",
+                30,
+                (2020, 10, 17),
+                100,
+                3000);
+            OutingContent bowlingOuting = new OutingContent(
+                "Bowling",
+                25,
+                (),
+                200,
+                5000);
+            OutingContent amusemnetOuting = new OutingContent(
+               "Amusement Park",
+               20,
+               (),
+               150,
+               3000);
+            OutingContent concertOuting = new OutingContent();
+      
+
+        }
+        public  int Addition(int totalEvent)
+        {
+            int result = totalEvent + totalEvent + totalEvent + totalEvent;
+            return result;
+        }
+    }
+}
