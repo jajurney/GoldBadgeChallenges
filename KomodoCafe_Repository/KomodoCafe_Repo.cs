819a9bc8f3d82784579ef3b5a2adf889c7570710
jajurney <josephajurney@gmail.com>
2020-10-30 22:01:53 -0400

    first commit

---
+++ KomodoCafe_Repository/KomodoCafe_Repo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoCafe_Repository
+{
+    public class KomodoCafe_Repo
+    {
+        private  List<MenuContent> _menuContents = new List<MenuContent>();
+
+        
+        public bool AddItemToMenu(MenuContent menuContent)
+        {
+            int itemCount = _menuContents.Count;
+            _menuContents.Add(menuContent);
+            bool itemAdded = (_menuContents.Count > itemCount) ? true : false;
+            return itemAdded;
+        }
+        public List<MenuContent> GetMenu()
+        {
+            return _menuContents;
+            
+        }
+       
+        public MenuContent GetItemByMealName(string menuItem)
+        {
+            foreach (MenuContent menuContent in _menuContents)
+            {
+                if (menuContent.MealItem.ToLower() == menuItem)
+                {
+                    return menuContent;
+                }
+            }
+            return null;
+        }
+       
+      
+        
+       
+        public bool UpdateMenuItems(string originalItem, MenuContent newItem)
+        {
+            MenuContent oldItem = GetItemByMealName(originalItem);
+            if (oldItem != null)
+            {
+                oldItem.MealNum = newItem.MealNum;
+                oldItem.MealItem = newItem.MealItem;
+                oldItem.MealDescription = newItem.MealDescription;
+                oldItem.MealPrice = newItem.MealPrice;
+                oldItem.Ingredients = newItem.Ingredients;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        public bool DeleteMenuItem(MenuContent existingItem)
+        {
+            bool deleteItem = _menuContents.Remove(existingItem);
+            return true;
+        }
+    }
+}
---
+++ KomodoCafe_Repository/MenuContent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoCafe_Repository
+{
+    public class MenuContent
+    {
+        public int MealNum { get; set; }
+        public string MealItem { get; set; }
+        public string MealDescription { get; set; }
+        public double MealPrice { get; set; }
+
+        public string Ingredients{ get; set; }
+
+
+        public MenuContent() { }
+        public MenuContent(int mealNum, string mealItem, string mealDescription, double mealPrice, string ingredient)
+        {
+            MealNum = mealNum;
+            MealItem = mealItem;
+            MealDescription = mealDescription;
+            MealPrice = mealPrice;
+            Ingredients = ingredient;
+        }
+
+      
+    }
+
+
+}
