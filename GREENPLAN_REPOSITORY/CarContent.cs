7cd5949e488e76a3257f7d235a8e6075e3157332
jajurney <josephajurney@gmail.com>
2020-10-30 21:58:34 -0400

    first commit

---
+++ GREENPLAN_REPOSITORY/CarContent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GREENPLAN_REPOSITORY
+{
+    public class CarContent
+    {
+        
+        public string PowerType { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set;}
+        public int Year { get; set; }
+
+        public CarContent() { }
+        public CarContent(string power, string make, string model, int year)
+        {
+            PowerType = power;
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+    }
+}
