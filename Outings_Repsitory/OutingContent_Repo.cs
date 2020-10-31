542437652758a5f625b79f457555b8c072cc2142
jajurney <josephajurney@gmail.com>
2020-10-30 22:04:46 -0400

    first commit

---
+++ Outings_Repsitory/OutingContent_Repo.cs
@@ -0,0 +1,58 @@
+﻿using Outings_Repsitory.Outings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outings_Repsitory
+{
+    public class OutingContent_Repo
+    {
+        private List<OutingContent> _contentList = new List<OutingContent>();
+
+        public bool AddOutingToList(OutingContent outingContent)
+        {
+            int startCount = _contentList.Count;
+            _contentList.Add(outingContent);
+            bool wasAdded = (_contentList.Count > startCount) ? true : false;
+            return wasAdded;
+        }
+        public List<OutingContent> GetOutingContents()
+        {
+            return _contentList;
+        }
+        public OutingContent GetOutingByType(string eventType)
+        {
+            foreach (OutingContent outingContent in _contentList)
+            {
+                if (outingContent.EventType.ToLower() == eventType.ToLower())
+                {
+                    return outingContent;
+                }
+
+            }
+            return null;
+        }
+        public bool UpdateExistingOutingContent(string originalTitle, OutingContent newContent)
+        {
+            OutingContent oldContent = GetOutingByType(originalTitle);
+            if (oldContent != null)
+            {
+                oldContent.EventType = newContent.EventType;
+                oldContent.PeopleAttended = newContent.PeopleAttended;
+                oldContent.EventDate = newContent.EventDate;
+                oldContent.TotalCostPerson = newContent.TotalCostPerson;
+                oldContent.TotalCostEvent = newContent.TotalCostEvent;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+       
+    }
+}
---
+++ Outings_Repsitory/Outings/OutingContent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Outings_Repsitory.Outings
+{
+    public class OutingContent
+    {
+        public string EventType { get; set; } 
+        public int PeopleAttended { get; set; }
+        public DateTime EventDate { get; set; } = new DateTime();
+        public int TotalCostPerson { get; set; }
+        public int TotalCostEvent { get; set; }
+
+       
+        
+        public OutingContent() { }
+        public OutingContent(string eventType, int peopleAtt, DateTime dateOfEvent, int totalPerson, int totalEvent)
+        {
+            EventType = eventType;
+            PeopleAttended = peopleAtt;
+            EventDate = dateOfEvent;
+            TotalCostPerson = totalPerson;
+            TotalCostEvent = totalEvent;
+        }
+        
+       
+       
+    }
+}
